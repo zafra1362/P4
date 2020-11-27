26ed839931daf55de08376c1c7c1ed78b19b05d0
Dylan <dzafrar01@elpuig.xeill.net>
2020-11-27 21:35:46 +0100

    acabat

--- src/xifrat/Main.java
+++ src/xifrat/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         //1.5
-        /*System.out.println("Text a xifrar:");
+        System.out.println("Text a xifrar:");
         String text = scanner.nextLine();
         byte[] txt = text.getBytes("UTF8");
         SecretKey secretKey = Xifrar.keygenKeyGeneration(256);
@@ -21,10 +21,10 @@ public class Main {
         System.out.println(txtx);
         byte[] txtd = Xifrar.decryptData(txtx, secretKey);
         String textx = new String(txtd, StandardCharsets.UTF_8);
-        System.out.println(textx);*/
+        System.out.println(textx);
 
         //1.6
-        /*System.out.print("Introdueix text: ");
+        System.out.print("Introdueix text: ");
         String textC = scanner.nextLine();
         System.out.print("Introdueix contrasenya: ");
         String password = scanner.nextLine();
@@ -32,10 +32,26 @@ public class Main {
         byte[] xifrarC = Xifrar.encryptData(textC.getBytes(), secretKeyPassword);
         System.out.println(xifrarC);
         byte[] desxifrarC = Xifrar.decryptData(xifrarC, secretKeyPassword);
-        System.out.println(new String(desxifrarC));*/
+        System.out.println(new String(desxifrarC));
 
         //1.7
+        System.out.println("getEncoded: "+ secretKey.getEncoded());
+        System.out.println("getAlgorithm: "+ secretKey.getAlgorithm());
+        System.out.println("getFormat: "+ secretKey.getFormat());
 
+        //1.8
+        SecretKey kg = Xifrar.keygenKeyGeneration(256);
+        System.out.println("Frase per xifrar: ");
+        String s = scanner.nextLine();
+        System.out.println("Contrasenya: ");
+        String contrasenyat = scanner.nextLine();
+        SecretKey ct = Xifrar.passwordKeyGeneration(contrasenyat, 256);
+        byte[] textM = s.getBytes("UTF8");
+        byte[] textX = Xifrar.encryptData(textM, ct);
+        System.out.println("Xifrat: " + textX);
+        byte[] textD = Xifrar.decryptData(textX, kg);
+        String textP = new String(textD, StandardCharsets.UTF_8);
+        System.out.println("Desxifrat: " + textP);
     }
 
 
